bb223c9f98fcbb610593220a322b4c4986e36a7d
katlumV <katlumpanda@gmail.com>
2019-04-17 07:21:33 -0400

    First commit

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/api/get_message')
+def get_message():
+    id = request.args.get('id')
+    name = request.args.get('name')
+    return jsonify({"messages from " + id: ["Hello there " + name]})
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
