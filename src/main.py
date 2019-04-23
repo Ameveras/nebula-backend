011b2954cce74a5456a3de515394d0b98544ecee
katlumV <katlumpanda@gmail.com>
2019-04-23 13:51:03 -0400

    Added dependencies in main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
+# Flask dependencies
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+#TODO: Add Base, Hotel
+from database_setup import Base, Hotel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Create session and connect to DB
+#TODO: Add database hotels
+engine = create_engine('sqlite:///nebula.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/api/get_message')
 def get_message():
