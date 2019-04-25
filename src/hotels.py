9fd5d99a6508b7851cabec3089d058bde8c4a49c
katlumV <katlumpanda@gmail.com>
2019-04-24 23:39:20 -0400

    Removed templated comments

--- src/hotels.py
+++ src/hotels.py
@@ -5,23 +5,15 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Hotel
 
 engine = create_engine('sqlite:///nebula.db')
-# Bind the engine to the metadata of the Base class so that the
-# declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-# A DBSession() instance establishes all conversations with the database
-# and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objects in the
-# session won't be persisted into the database until you call
-# session.commit(). If you're not happy about the changes, you can
-# revert all of them back to the last commit by calling
-# session.rollback()
 session = DBSession()
 
 
 # Menu for UrbanBurger
-newHotel = Hotel(idHotel = 1, hNombre = "NebulaBase", direccion = "Santo Domingo, Dominican Republic", pais = "Dominican Republic", telefono = "809-000-0001", email = "nebula@gmail.com")
+newHotel = Hotel(idHotel=1, hNombre="NebulaBase", direccion="Santo Domingo, Dominican Republic",
+                 pais="Dominican Republic", telefono="809-000-0001", email="nebula@gmail.com")
 
 session.add(newHotel)
 session.commit()
