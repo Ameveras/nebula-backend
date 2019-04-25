c1e57d31cb84fa4a29e759d95417c5cef3b6c709
katlumV <katlumpanda@gmail.com>
2019-04-24 20:40:31 -0400

    Fixed some bugs in main.py

--- src/main.py
+++ src/main.py
@@ -2,23 +2,22 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-#TODO: Add Base, Hotel
 from database_setup import Base, Hotel
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Create session and connect to DB
-#TODO: Add database hotels
 engine = create_engine('sqlite:///nebula.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/api/hotels/add-hotel', methods = ['GET'])
-def get_message():
+def get_hotel():
+    if not 'id' in request.args:
+        return jsonify({"error": "ID not specified"})
     id = request.args.get('id')
-    name = request.args.get('name')
-    query = session.query(Hotel).filter(Hotel.hNombre == name)
+    query = session.query(Hotel).filter(Hotel.idHotel == id)
     result = query.all()
     return jsonify({"messages from " + id: ["Hello there " + name]})
 
