62724916ac43189b2a1881e6d90e51fd688d3ad3
katlumV <katlumpanda@gmail.com>
2019-05-14 15:02:30 -0400

    Added the User class

--- src/database_setup.py
+++ src/database_setup.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import datetime
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -16,6 +17,23 @@ class Hotel(Base):
     pais = Column(String(250), nullable=True)
     telefono = Column(String(25), nullable=True)
     email = Column(String(250), nullable=True)
+
+
+class Usuario(Base):
+    __tablename__ = 'Usuario'
+    IdCliente = Column(Integer, primary_key=True, nullable=True)
+    TipoDoc = Column(String(100), Nulable=True)
+    Documento = Column(String(100), nullable=True)
+    CNombre = Column(String(100), nullable=True)
+    CApellido = Column(String(100), nullable=True)
+    Sexo = Column(String(10), nullable=True)
+    Pais = Column(String(100), nullable=True)
+    FechaNacimiento = Column(datetime.date, nullable=True)
+    Telefono = Column(String(15), nullable=True)
+    Email = Column(String(15), nullable=True)
+    Contraseña = Column(String(15), nullable=True)
+    Foto = Column(String(15), nullable=True)
+    FechaRegistro = Column(datetime.date, nullable=True)
 
 
 engine = create_engine('sqlite:///nebula.db')
